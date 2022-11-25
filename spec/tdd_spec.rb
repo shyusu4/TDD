96ab67db3d66cc2704ab9901e1637894afe4bdaf
melashu <meshu102@outlook.com>
2022-11-25 13:42:54 +0300

    Add spec for factorial

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -1,31 +1,34 @@
-
 describe Solver do
-    context "testing reverse method" do
-        it "should reverse the string" do
-            solver = Solver.new
-            expect(solver.reverse('hello')).to eq('olleh')
-        end
+  context 'testing reverse method' do
+    it 'should reverse the string' do
+      solver = Solver.new
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+  end
+
+  context 'testing fizzbuzz method' do
+    it 'should return fizz' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(3)).to eq('fizz')
     end
 
-    context "testing fizzbuzz method" do
-        it "should return fizz" do
-            solver = Solver.new
-            expect(solver.fizzbuzz(3)).to eq('fizz')
-        end
-
-        it "should return buzz" do
-            solver = Solver.new
-            expect(solver.fizzbuzz(35)).to eq('buzz')
-        end
-
-        it "should return fizzbuzz" do
-            solver = Solver.new
-            expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
-        end
-
-        it "should return number" do
-            solver = Solver.new
-            expect(solver.fizzbuzz(7)).to eq(7)
-        end
+    it 'should return buzz' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(35)).to eq('buzz')
     end
+
+    it 'should return fizzbuzz' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'should return number' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq(7)
+    end
+  end
+  it 'Factorial method should return 120 when we pass 5' do
+    solver = Solver.new
+    expect(solver.factorial(5)).to eql 120
+  end
 end
