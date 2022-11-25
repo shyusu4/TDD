7877c76a3e8053cc3dd6365f060970f8caafbe8c
melashu <meshu102@outlook.com>
2022-11-25 13:53:02 +0300

    Add Solver implemetion

---
+++ solver.rb
@@ -0,0 +1,35 @@
+class Solver
+  def factorial(value)
+    return 1 if value.zero?
+
+    value * factorial(value - 1)
+  end
+
+  def reverse(str)
+    return str[0] if str.length == 1
+
+    reverse(str.slice(1, str.length - 1)) + str[0]
+  end
+
+  def fizzbuzz(val)
+    if (val % 3).zero? && (val % 5).zero?
+
+      'fizzbuzz'
+
+    elsif (val % 3).zero?
+
+      'fizz'
+
+    elsif (val % 5).zero?
+
+      'buzz'
+
+    else
+
+      val.to_s
+
+    end
+  end
+end
+
+
